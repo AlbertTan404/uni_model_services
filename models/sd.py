839b23d40239b71d2cfce0ec67c11122d4dcdd50
AlbertTan <albert_twh@qq.com>
2024-01-04 08:57:56 -0800

    fix sd

--- models/sd.py
+++ models/sd.py
@@ -1,6 +1,5 @@
 import torch
 import diffusers
-from diffusers import StableDiffusionPipeline
 
 from models.base import WrappedBASE
 
@@ -8,7 +7,17 @@ from models.base import WrappedBASE
 class WrappedSD(WrappedBASE):
     def __init__(self, name_or_path, outputs_dir, device, **kwargs) -> None:
         super().__init__(name_or_path, outputs_dir, device)
-        self.pipe = StableDiffusionPipeline.from_pretrained(pretrained_model_name_or_path=name_or_path, torch_dtype=torch.float16, **kwargs).to(device)
+
+        pipe_cls = self.get_pipe_cls(name_or_path)
+
+        self.pipe = pipe_cls.from_pretrained(pretrained_model_name_or_path=name_or_path, torch_dtype=torch.float16, **kwargs).to(device)
+    
+    @staticmethod
+    def get_pipe_cls(name_or_path: str):
+        if 'stable-diffusion-xl' in name_or_path:
+            return diffusers.StableDiffusionXLPipeline
+        else:
+            return diffusers.StableDiffusionPipeline
 
     def switch_scheduler(self, scheduler_name: str):
         scheduler_cls = getattr(diffusers, scheduler_name)
